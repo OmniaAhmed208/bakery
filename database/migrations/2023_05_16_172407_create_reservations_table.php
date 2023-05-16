864459d1a781a6a58645be0e6fb2766a093ff9b2
OmniaAhmed208 <oa66494@gmail.com>
2023-05-16 23:08:31 +0300

    install laravel and admin dashboard

---
+++ database/migrations/2023_05_16_172407_create_reservations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('email');
+            $table->string('tel_number');
+            $table->dateTime('res_date'); // reservation date
+            $table->unsignedBigInteger('table_id');
+            $table->integer('guest_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
